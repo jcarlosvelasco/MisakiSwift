6f85323d8efc7656254258f527723899e5a828f9
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2026-05-16 12:26:34 +0200

    Update mlx-swift to 0.31.x and MLXUtilsLibrary to 0.0.7

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/ml-explore/mlx-swift", exact: "0.30.2"),
-    .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", exact: "0.0.6")
+    .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.31.3")),
+    .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", exact: "0.0.7")
   ],
   targets: [
     .target(
